18720d830becffb70bcd0f5df6ec9a7d72668f6d
00014905wiut <118118342+00014905wiut@users.noreply.github.com>
2024-02-15 23:04:05 +0500

    Migration created

---
+++ Events Manager 14905/Migrations/20240215180326_First Migration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Events_Manager_14905.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    EventId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EventName = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    EventDescription = table.Column<string>(type: "nvarchar(200)", nullable: false),
+                    EventCategory = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    UserPhoneNum = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    EventCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EventFor = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.EventId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
---
+++ Events Manager 14905/Models/Event.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Tracing;
+
+namespace Events_Manager_14905.Models
+{
+    public class Event
+    {
+        [Key] // Marking EventId as the primary key
+        public int EventId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string EventName { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(200)")]
+        public string EventDescription { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string EventCategory { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string UserName { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string UserPhoneNum { get; set; } = "";
+
+        public DateTime EventCreated { get; set; } = DateTime.Now;
+
+        public DateTime EventFor { get; set; }
+
+        public int TimeLeft()
+        {
+            long tmlft = (EventFor.Ticks - EventCreated.Ticks) / TimeSpan.TicksPerSecond;
+            return (int)tmlft;
+        }
+    }
+}
